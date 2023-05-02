2060b075c66d814b6406c9c8d9c3dfb356793834
Chris Hale <87267417+waymakr@users.noreply.github.com>
2023-05-02 16:10:40 +1000

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Header')
-streamlit.text('text1')
-streamlit.text('text2')
-streamlit.text('text3')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
